236740a6fc1b1416da1ec1db7dfc65cc400d48e2
lancelot343 <60350575+lancelot343@users.noreply.github.com>
2020-02-11 00:03:38 +0200

    Update homework_1.js

--- homework_1.js
+++ homework_1.js
@@ -1,30 +1,30 @@
-const let balls = 15.678;
-const let oranges = 123.965;
-const let pineapples = 90.2345;
+const balls = 15.678;
+const oranges = 123.965;
+const pineapples = 90.2345;
 
-const let max = Math.max(balls, oranges, pineapples);
+const max = Math.max(balls, oranges, pineapples);
 
-const let min = Math.min(balls, oranges, pineapples)
+const min = Math.min(balls, oranges, pineapples)
 
-const let sum = balls + oranges + pineapples;
+const sum = balls + oranges + pineapples;
 
-const let smaller_prices = Math.floor(balls) + Math.floor(oranges) + Math.floor(pineapples);
+const smaller_prices = Math.floor(balls) + Math.floor(oranges) + Math.floor(pineapples);
 
-const let smaller_sum = Math.ceil(sum/100)*100;
+const smaller_sum = Math.ceil(sum/100)*100;
 
 const res = smaller_sum % 2 == 0;
 
-const let change = 500.0 - sum;
+const change = 500.0 - sum;
 
-const let average = (sum/3).toFixed(2)
+const average = (sum/3).toFixed(2)
 
-const let discount = Math.random().toFixed(2);
+const discount = Math.random().toFixed(2);
 
-const let product = oranges + pineapples;
+const product = oranges + pineapples;
 
-const let price = Math.trunc((product-(product*discount)));
+const price = Math.trunc((product-(product*discount)));
 
-const let missed_cash = Math.abs(Math.floor((product/2) - price));
+const missed_cash = Math.abs(Math.floor((product/2) - price));
 
 console.log
 (`
